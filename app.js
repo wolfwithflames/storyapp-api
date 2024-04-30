0710fb418c162deb3ee9c11ff9873abbc23f1458
Wolf With Flames <tailorkeyur44@gmail.com>
2024-04-30 13:38:05 +0530

    1. Image url sanetization removed

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+
 //To allow cross-origin requests
 app.use(cors());
 
--- controllers/StoryController.js
+++ controllers/StoryController.js
@@ -1,74 +1,83 @@
 const Story = require("../models/StoryModel");
-const { body,validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const { sanitizeBody } = require("express-validator");
 const apiResponse = require("../helpers/apiResponse");
 const auth = require("../middlewares/jwt");
 var mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
-const ObjectId = require('mongodb').ObjectId;
-
-
+const ObjectId = require("mongodb").ObjectId;
 
 // Book Schema
 function StoryData(data) {
 	this.id = data._id;
-	this.title= data.title;
+	this.title = data.title;
 	this.description = data.description;
-    this.imageUrl = data.imageUrl;
+	this.imageUrl = data.imageUrl;
 	this.createdAt = data.createdAt;
 }
 
-
 /**
  * Story Add.
- * 
- * @param {string}      title 
+ *
+ * @param {string}      title
  * @param {string}      description
  * @param {string}      imageUrl
- * 
+ *
  * @returns {Object}
  */
 exports.storyAdd = [
 	auth,
 	body("title", "Title must not be empty.").isLength({ min: 1 }).trim(),
-	body("description", "Description must not be empty.").isLength({ min: 1 }).trim(),
-	body("imageUrl").optional().isURL().withMessage('Must be a URL'),
-	
-	sanitizeBody("*").escape(),
+	body("description", "Description must not be empty.")
+		.isLength({ min: 1 })
+		.trim(),
+	body("imageUrl").optional().isURL().withMessage("Must be a URL"),
+
+	sanitizeBody("title").escape(),
+	sanitizeBody("description").escape(),
 	(req, res) => {
 		try {
 			const errors = validationResult(req);
-			var story = new Story(
-				{ title: req.body.title,
-					user: req.user,
-					description: req.body.description,
-					imageUrl: req.body.imageUrl,
-					createdBy: req.user._id,
-				});
+			var story = new Story({
+				title: req.body.title,
+				user: req.user,
+				description: req.body.description,
+				imageUrl: req.body.imageUrl,
+				createdBy: req.user._id,
+			});
 
 			if (!errors.isEmpty()) {
-				return apiResponse.validationErrorWithData(res, "Validation Error.", errors.array());
-			}
-			else {
-				//Save book.
+				return apiResponse.validationErrorWithData(
+					res,
+					"Validation Error.",
+					errors.array()
+				);
+			} else {
+				//Save story.
 				story.save(function (err) {
-					if (err) { return apiResponse.ErrorResponse(res, err); }
+					if (err) {
+						return apiResponse.ErrorResponse(res, err);
+					}
 					let storyData = new StoryData(story);
-					return apiResponse.successResponseWithData(res,"Story add Success.", storyData);
+					return apiResponse.successResponseWithData(
+						res,
+						"Story add Success.",
+						storyData
+					);
 				});
 			}
 		} catch (err) {
-            console.log(err);
-			//throw error in json response with status 500. 
+			console.log(err);
+			//throw error in json response with status 500.
 			return apiResponse.ErrorResponse(res, err);
 		}
-	}
+	},
 ];
 
 /**
-* getWeekStories.
-* @returns {Object}
-*/
+ * getWeekStories.
+ * @returns {Object}
+ */
 exports.getWeekStories = [
 	auth,
 	(req, res) => {
@@ -83,52 +92,54 @@ exports.getWeekStories = [
 			console.log(currentDate, pastDate);
 
 			const query = {
-                createdAt: {
-                    $lte: currentDate,
-                    $gte: pastDate
-                },
-				createdBy: ObjectId(req.user._id)
-            };
-            const aggregation = [
-				{ $match: query },				
+				createdAt: {
+					$lte: currentDate,
+					$gte: pastDate,
+				},
+				createdBy: ObjectId(req.user._id),
+			};
+			const aggregation = [
+				{ $match: query },
 				{
 					$group: {
 						_id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
-						documents: { $push: "$$ROOT" } // Push the whole document into an array
-					}
+						documents: { $push: "$$ROOT" }, // Push the whole document into an array
+					},
 				},
-				{ 
+				{
 					$group: {
 						_id: null,
 						documents: {
 							$push: {
-								"date": "$_id",
-								"stories": "$documents"
-							}
-						}
-					}
+								date: "$_id",
+								stories: "$documents",
+							},
+						},
+					},
 				},
 				{
-					$unwind: "$documents"
+					$unwind: "$documents",
 				},
 				{
-					$replaceRoot: { "newRoot": "$documents" }
+					$replaceRoot: { newRoot: "$documents" },
 				},
-				{ 
-					$sort: { date: -1 } // Sorting by createdAt field in ascending order
-				}
+				{
+					$sort: { date: -1 }, // Sorting by createdAt field in ascending order
+				},
 			];
-			
-            
-            Story.aggregate(aggregation, function(err, results) {
-                if (err) return apiResponse.ErrorResponse(res, err);
-                return apiResponse.successResponseWithData(res,"Story add Success.", results);
-            });
-			
+
+			Story.aggregate(aggregation, function (err, results) {
+				if (err) return apiResponse.ErrorResponse(res, err);
+				return apiResponse.successResponseWithData(
+					res,
+					"Story add Success.",
+					results
+				);
+			});
 		} catch (err) {
-            console.log(err);
-			//throw error in json response with status 500. 
+			console.log(err);
+			//throw error in json response with status 500.
 			return apiResponse.ErrorResponse(res, err);
 		}
-	}
+	},
 ];
